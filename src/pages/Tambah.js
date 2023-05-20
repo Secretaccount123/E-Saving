e21a7e5c1b21efd8581e4fbb17892761b5ed6a7e
Secretaccount123 <134087991+Secretaccount123@users.noreply.github.com>
2023-05-20 13:38:49 +0700

    Update Tambah.js

--- src/pages/Tambah.js
+++ src/pages/Tambah.js
@@ -38,29 +38,31 @@ const Tambah = ({ celengans, setCelengans }) => {
 
     return (
         <div className='h-screen flex justify-center'>
-            <form onSubmit={handleSubmit} className="flex flex-col justify-center h-72 mt-24 gap-4 p-8 rounded-md shadow-md bg-slate-500">
+            <form onSubmit={handleSubmit} className="flex flex-col text-gray-200 justify-center h-72 mt-24 gap-4 p-8 rounded-2xl shadow-md bg-slate-500">
                 <div className='w-full flex justify-start '>
-                <button onClick={e=>navigate(-1)}>
-                    Back
-                </button>
+                    <button onClick={e => navigate(-1)}>
+                        Back
+                    </button>
                 </div>
-                
-                <div className='flex space-x-2'>
-                    <p>Nama</p>
-                    <input className='rounded-md p-1 bg-transparent border' onChange={(e) => setNama(e.target.value)} name='nama' type='text' placeholder='Nama tabungan' required />
-                </div>
+                <div className='space-y-3'>
 
-                <div className='flex space-x-2'>
-                    <p>Target</p>
-                    <input className='rounded-md p-1 bg-transparent border' onChange={(e) => setTarget(e.target.value)} name='target' type='number' placeholder='Target Tabungan' required />
-                </div>
-                <div className='flex space-x-2'>
-                    <p>Nominal</p>
-                    <input className='rounded-md p-1 bg-transparent border' onChange={(e) => setNominal(e.target.value)} name='nominal' type='number' placeholder='Nominal Pengisian' required />
+                    <div className='flex space-x-2 justify-between'>
+                        <p>Nama</p>
+                        <input className='pb-1 bg-transparent border-0 border-b-2 focus:outline-none focus:ring-0 focus:border-black border-gray-200' onChange={(e) => setNama(e.target.value)} name='nama' type='text' placeholder='Nama tabungan' required />
+                    </div>
+
+                    <div className='flex space-x-2 justify-between'>
+                        <p>Target</p>
+                        <input className='pb-1 bg-transparent border-0 border-b-2 focus:outline-none focus:ring-0 focus:border-black border-gray-200' onChange={(e) => setTarget(e.target.value)} name='target' type='number' placeholder='Target Tabungan' required />
+                    </div>
+                    <div className='flex space-x-2 justify-between'>
+                        <p>Nominal</p>
+                        <input className='pb-1 bg-transparent border-0 border-b-2 focus:outline-none focus:ring-0 focus:border-black border-gray-200' onChange={(e) => setNominal(e.target.value)} name='nominal' type='number' placeholder='Nominal Pengisian' required />
+                    </div>
                 </div>
                 <div>
                     <p>Rencana Pengisian</p>
-                    <select className='rounded-md p-1 bg-transparent border' onChange={(e) => setRencana(e.target.value)} value={rencana} name="cars" id="cars" required>
+                    <select className='pb-1 bg-transparent border-0 border-b-2 focus:outline-none focus:ring-0 focus:border-black border-gray-200' onChange={(e) => setRencana(e.target.value)} value={rencana} required>
                         <option value="0">Harian</option>
                         <option value="1">Mingguan</option>
                         <option value="2">Bulanan</option>
